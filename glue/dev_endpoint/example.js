372f04d5c0f173ea3867bb4865694b9f9ec48a61
CaptainM <37575000+hlmiao@users.noreply.github.com>
2020-09-21 17:21:48 +0800

    Create example.js

---
+++ glue/dev_endpoint/example.js
@@ -0,0 +1,20 @@
+{
+    "EndpointName": "test001",
+    "RoleArn": "arn:aws-cn:iam::xxxxxxx:role/Glue_Endpoint_Admin",
+    "SecurityGroupIds": [
+        "sg-xxxxxx"
+    ],
+    "SubnetId": "subnet-xxxxxxxxxxxxxx",
+    "PublicKey":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDT1v yuornickname@mail.com",
+    "WorkerType": "G.1X",
+    "GlueVersion": "1.0",
+    "NumberOfWorkers": 2,
+    "ExtraPythonLibsS3Path": "",
+    "ExtraJarsS3Path": "",
+    "Tags": {
+        "KeyName": ""
+    },
+    "Arguments": {
+        "KeyName": ""
+    }
+}
